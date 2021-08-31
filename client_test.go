609e44de8e3d5ea9935d607f0660355ffa92e89c
j33ty <radhe@sanity.io>
2021-08-31 15:02:44 +0200

    Fix bug where Accept header is being set twice

--- client_test.go
+++ client_test.go
@@ -16,6 +16,8 @@ func TestAuth(t *testing.T) {
 	withSuite(t, func(s *Suite) {
 		s.mux.Get("/v1/data/query/myDataset", func(w http.ResponseWriter, r *http.Request) {
 			assert.Equal(t, "Bearer bork", r.Header.Get("Authorization"))
+			assert.Equal(t, "bar", r.Header.Get("foo"))
+			assert.Equal(t, []string{"application/json", "text/xml"}, r.Header.Values("accept"))
 
 			w.WriteHeader(http.StatusOK)
 			_, err := w.Write(mustJSONBytes(&api.QueryResponse{
@@ -27,5 +29,11 @@ func TestAuth(t *testing.T) {
 
 		_, err := s.client.Query("*").Do(context.Background())
 		require.NoError(t, err)
-	}, sanity.WithToken("bork"))
+	},
+		sanity.WithToken("bork"),
+		sanity.WithHTTPHeaders(map[string]string{
+			"foo":    "bar",
+			"accept": "text/xml",
+		}),
+	)
 }
