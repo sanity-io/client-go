5853c0ecd57e39db16ff0b2ed2717daa7092cf1b
j33ty <radhe@sanity.io>
2021-09-10 12:40:50 +0200

    When no document specified, return empty response from GET documents API

--- get_documents.go
+++ get_documents.go
@@ -5,7 +5,6 @@ import (
 	"strings"
 
 	"github.com/sanity-io/client-go/api"
-	"github.com/sanity-io/client-go/internal/requests"
 )
 
 // GetDocuments returns a new GetDocuments builder.
@@ -20,13 +19,14 @@ type GetDocumentsBuilder struct {
 }
 
 // Do performs the query.
-// On validation failure, this will return an error of *InvalidRequestError.
 // On API request failure, this will return an error of type *RequestError.
 func (b *GetDocumentsBuilder) Do(ctx context.Context) (*api.GetDocumentsResponse, error) {
-	req, err := b.buildGET()
-	if err != nil {
-		return nil, err
+	if len(b.docIDs) == 0 {
+		return &api.GetDocumentsResponse{}, nil
 	}
+
+	req := b.c.newAPIRequest().
+		AppendPath("data/doc", b.c.dataset, strings.Join(b.docIDs, ","))
 
 	var resp api.GetDocumentsResponse
 	if _, err := b.c.do(ctx, req, &resp); err != nil {
@@ -34,18 +34,4 @@ func (b *GetDocumentsBuilder) Do(ctx context.Context) (*api.GetDocumentsResponse
 	}
 
 	return &resp, nil
-}
-
-func (b *GetDocumentsBuilder) buildGET() (*requests.Request, error) {
-	if len(b.docIDs) == 0 {
-		return nil, &InvalidRequestError{Description: "no document ID specified"}
-	}
-
-	req := b.c.newAPIRequest().
-		AppendPath("data/doc", b.c.dataset, strings.Join(b.docIDs, ","))
-
-	if len(req.EncodeURL()) > maxGETRequestURLLength {
-		return nil, &InvalidRequestError{Description: "max URL length exceeded"}
-	}
-	return req, nil
 }
